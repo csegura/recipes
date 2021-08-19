fbcc24712e5cc7b382b3242b33b181f5c4ae2604
csegura <romheat@gmail.com>
2021-08-19 22:51:14 +0200

    dmenu monitor selection

--- dmonitor.sh
+++ dmonitor.sh
@@ -12,11 +12,12 @@ extramonitor() {
       "external") xrandr --output "$main" --auto --right-of "$aux" --output "$aux" --auto --output "$laptop" --off ;;
       "all") xrandr --output "$main" --auto --right-of "$aux" --output "$aux" --auto --output "$laptop" --right-of "$aux" --auto ;;
       "allthree") xrandr --output "$aux" --mode 1920x1080 && xrandr --output "$main" --mode 1920x1080 && xrandr --output "$laptop" --auto --output "$aux" --left-of "$laptop" --output "$main" --right-of "$laptop";;
-      "duplicate") xrandr --output "$main" --set audio force-dvi --mode 1920x1080 && xrandr --output "$laptop" --auto --output "$main" --same-as "$laptop" ;;
+      "duplicate") xrandr --output "$main" --mode 1920x1080 && xrandr --output "$laptop" --auto --output "$main" --same-as "$laptop" ;;
       *) notify-send "Multi Monitor" "Unknown Operation" ;;
   esac
 }
 
-selection=$(printf "laptopoff\nlaptopon\nexternal\nall" | dmenu -i -p "Monitor Configuration")
+selection=$(printf "laptopon\nlaptopoff\nexternal\nall" | dmenu -i -p "Monitor Configuration")
+#${selection:="laptopon"}
 extramonitor "$selection" 
 # && fixdualhead -pd "1 2 3 4 5 6 7" -p "$laptop" -s "$aux" -sd "8 9 0"
